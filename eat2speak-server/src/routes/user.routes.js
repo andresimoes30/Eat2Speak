a3ee9953269468bd1acb7100df5a0a12eb6e48bd
Falty <dinissilva555@gmail.com>
2025-06-01 19:05:37 +0200

    fix: correct middleware import name from verifyToken to verifyAuthToken

--- eat2speak-server/src/routes/user.routes.js
+++ eat2speak-server/src/routes/user.routes.js
@@ -7,13 +7,13 @@ const {
   updateProfile
 } = require('../controllers/user.controller');
 
-const { verifyToken } = require('../middlewares/auth.middleware');
+const { verifyAuthToken } = require('../middlewares/auth.middleware');
 
 // Rota para registrar novo usuário
 router.post('/register', registerUser);
 
 // Rotas protegidas
-router.get('/me', verifyToken, getProfile);
-router.put('/me', verifyToken, updateProfile);
+router.get('/me', verifyAuthToken, getProfile);
+router.put('/me', verifyAuthToken, updateProfile);
 
 module.exports = router;
